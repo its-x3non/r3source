d394832412af1143c4d8166a0a80db790f56a6ce
Glitch (r3source) <none>
2023-12-17 19:41:37 +0000

    Edit Footer to include View

--- src/app.jsx
+++ src/app.jsx
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Router, Link } from "wouter";
 
 /**
-* This code defines the react app
-*
-* Imports the router functionality to provide page navigation
-* Defines the Home function outlining the content on each page
-* Content specific to each page (Home and About) is defined in their components in /pages
-* Each page content is presented inside the overall structure defined here
-* The router attaches the page components to their paths
-*/
+ * This code defines the react app
+ *
+ * Imports the router functionality to provide page navigation
+ * Defines the Home function outlining the content on each page
+ * Content specific to each page (Home and About) is defined in their components in /pages
+ * Each page content is presented inside the overall structure defined here
+ * The router attaches the page components to their paths
+ */
 
 // Import and apply CSS stylesheet
 import "./styles/styles.css";
@@ -18,7 +18,7 @@ import "./styles/styles.css";
 import PageRouter from "./components/router.jsx";
 
 // The component that adds our Meta tags to the page
-import Seo from './components/seo.jsx';
+import Seo from "./components/seo.jsx";
 
 // Home function that is reflected across the site
 export default function Home() {
@@ -38,14 +38,12 @@ export default function Home() {
           <span className="divider">|</span>
           <Link href="/about">About</Link>
         </div>
-        <a
-          className="btn--remix"
-          target="_top"
-          href="https://glitch.com/edit/#!/remix/glitch-hello-react"
-        >
-          <img src="https://cdn.glitch.com/605e2a51-d45f-4d87-a285-9410ad350515%2FLogo_Color.svg?v=1618199565140" alt="" />
-          Remix on Glitch
-        </a>
+        <div className="viewOn">
+          <Link href="https://glitch.com/edit/#!/remix/glitch-hello-react">Remix on Glitch</Link>
+          <span className="divider">|</span>
+          <Link href="https://github.com/its-x3non/r3source">View on Github</Link>
+        </div>
+          
       </footer>
     </Router>
   );
